875390e694a6982fc3dba840f751b513259058d4
Isakiye Afasha <afashaisakiye@gmail>
2020-11-03 03:47:18 +0300

    wp_register_style was called incorrectly notice

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -1,5 +1,10 @@
 <?php 
 
-wp_register_style( 'register_style', smartup_plugin_dir_url().'/css/register.css', array(), '1.0.0', 'all' );
 
-wp_register_script( 'register_script', smartup_plugin_dir_url().'/js/register.js' , array(), true );
+
+function suf_plugin_enqueue(){
+  wp_register_style( 'register_style', smartup_plugin_dir_url().'/css/register.css', array(), '1.0.0', 'all' );
+
+wp_register_script( 'register_script', smartup_plugin_dir_url().'/js/register.js' , array(), true );  
+}
+add_action( 'wp_enqueue_scripts', 'suf_plugin_enqueue' );
